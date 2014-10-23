7f5f7717cf867102989bddc9e8e7d4c086849ade
daishi sasaki <daishi.1980@gmail.coom>
2014-10-24 08:27:12 +0900

    線形探索(逐次探索)

---
+++ linear_search.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linear_search
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var strarray = new string[]{ "A", "B", "C", "D", "E" };
+            Console.WriteLine( Search( "D", strarray ) );
+
+            Console.ReadLine();
+        }
+
+        static private int Search( string word, string[] array )
+        {
+            for ( int ii = 0; ii < array.Length; ii++ )
+                if ( array[ii] == word )
+                    return ii;
+
+            return -1;
+        }
+    }
+}
